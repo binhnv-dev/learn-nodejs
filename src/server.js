15eb9e0a54535acfb9d28fcd82b389f98e54f3ca
binhnv <binh46607@gmail.com>
2026-06-14 04:14:15 +0700

    [feat]: build struct

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+require('dotenv').config()
+
+const app = express();
+
+const {PORT} = process.env
+
+app.listen(PORT, () =>{
+  console.log('Start BE with port:', PORT)
+})
